30c1b9b9917d9d51e82f7c542ccc3588d309753a
RaulWei <674877404@qq.com>
2015-08-04 20:23:34 +0800

    update app urls

--- myenrich/enrich/urls.py
+++ myenrich/enrich/urls.py
@@ -1,18 +1,16 @@
 __author__ = 'WMW'
 
 # system import
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # user import
-#from enrich.views import enrich
 import enrich.views
 
 urlpatterns = patterns('',
-    url(r'^milestone/', enrich.views.enrich),
-    url(r'^milestone_history/', enrich.views.milestone_history),
-    #url(r'^test', enrich.views.test),
-    url(r'^milestone_history_msBox_record', enrich.views.milestone_history_msBox_record),
-    url(r'^ajax/deletemiles', enrich.views.delete_milestone), 
-    url(r'^ajax/updatemiles', enrich.views.update_milestone),
-)
+                       url(r'^milestone', enrich.views.enrich),
+                       url(r'^milestone_history', enrich.views.milestone_history),
+                       url(r'^milestone_history_msBox_record', enrich.views.milestone_history_msBox_record),
+                       url(r'^ajax/deletemiles', enrich.views.delete_milestone),
+                       url(r'^ajax/updatemiles', enrich.views.update_milestone),
+                       )
 
