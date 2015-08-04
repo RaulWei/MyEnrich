3d0fc01dfffe32f9cdf74fe53260106785ff6c01
RaulWei <674877404@qq.com>
2015-08-04 17:15:22 +0800

    update views.py

--- myenrich/enrich/views.py
+++ myenrich/enrich/views.py
@@ -40,10 +40,14 @@ def milestone_history_msBox_record(request):
     c = Context({'milestones': ms})
     return HttpResponse(t.render(c))
 
+
+
 def delete_milestone(request):
     id = request.REQUEST["id"]
     Milestone.objects.filter(id = id).delete()
     return HttpResponse('true')
+
+
 def update_milestone(request):
     id = request.REQUEST["id"]
     content = request.REQUEST["content"]
