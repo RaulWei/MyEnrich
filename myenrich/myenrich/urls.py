20936105461bd57fbfa394e3c851a9601f9a1da5
RaulWei <674877404@qq.com>
2015-08-04 20:19:55 +0800

    reformat urls

--- myenrich/myenrich/urls.py
+++ myenrich/myenrich/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myenrich.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^enrich/', include('enrich.urls')),
-)
+                       # Examples:
+                       # url(r'^$', 'myenrich.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^enrich/', include('enrich.urls')),
+                       )
