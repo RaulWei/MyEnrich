4b847351385915723b2bdc4a9f939c7d00bbb77c
RaulWei <674877404@qq.com>
2015-08-04 20:24:38 +0800

    update models.py

--- myenrich/enrich/models.py
+++ myenrich/enrich/models.py
@@ -6,7 +6,10 @@ from django.contrib import admin
 class Milestone(models.Model):
     Content = models.CharField(max_length=500)
     Date = models.CharField(max_length=200)
+
+
 class MilestoneAdmin(admin.ModelAdmin):
-    list_display = ('id','Date', 'Content')
+    list_display = ('id', 'Date', 'Content')
+
 
 admin.site.register(Milestone, MilestoneAdmin)
