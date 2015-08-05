6f7d94971acec43c51c3bb85767bd91384b0c10a
RaulWei <674877404@qq.com>
2015-08-05 18:27:04 +0800

    update settings.py sae

--- myenrich/myenrich/settings.py
+++ myenrich/myenrich/settings.py
@@ -8,6 +8,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# sae need
+# import os.path
+# import sae.const
+# from os import environ
+#
+# MYSQL_DB = sae.const.MYSQL_DB
+# MYSQL_USER = sae.const.MYSQL_USER
+# MYSQL_PASS = sae.const.MYSQL_PASS
+# MYSQL_HOST_M = sae.const.MYSQL_HOST
+# MYSQL_HOST_S = sae.const.MYSQL_HOST_S
+# MYSQL_PORT = sae.const.MYSQL_PORT
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -68,6 +80,14 @@ DATABASES = {
         #'PASSWORD': '',
         #'HOST': '',
         #'PORT': '3306',
+
+        # sae mysql
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': MYSQL_DB,
+        # 'USER': MYSQL_USER,
+        # 'PASSWORD': MYSQL_PASS,
+        # 'HOST': MYSQL_HOST_M,
+        # 'PORT': MYSQL_PORT,
     }
 }
 
